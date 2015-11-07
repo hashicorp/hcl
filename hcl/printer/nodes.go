0704c168e18bf3c7113ae2d5967e68cc2c1ab6d9
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-11-07 15:15:40 -0800

    hcl/printer: make boolean logic one line

--- hcl/printer/nodes.go
+++ hcl/printer/nodes.go
@@ -178,11 +178,9 @@ func (p *printer) objectItem(o *ast.ObjectItem) []byte {
 		buf.WriteByte(blank)
 
 		// reach end of key
-		if i == len(o.Keys)-1 {
-			if o.Assign.IsValid() && len(o.Keys) == 1 {
-				buf.WriteString("=")
-				buf.WriteByte(blank)
-			}
+		if o.Assign.IsValid() && i == len(o.Keys)-1 && len(o.Keys) == 1 {
+			buf.WriteString("=")
+			buf.WriteByte(blank)
 		}
 	}
 
