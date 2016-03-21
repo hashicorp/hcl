9832b69cfd15ff9ef5fd16abec774629883de031
James Nugent <james@jen20.com>
2016-03-21 00:17:10 +0000

    Fix line endings in hclfmt test

--- hcl/fmtcmd/fmtcmd_test.go
+++ hcl/fmtcmd/fmtcmd_test.go
@@ -11,6 +11,8 @@ import (
 	"sort"
 	"syscall"
 	"testing"
+
+	"github.com/hashicorp/hcl/testhelper"
 )
 
 var fixtureExtensions = []string{"hcl"}
@@ -324,6 +326,8 @@ func TestRunDiff(t *testing.T) {
 		}
 	}
 
+	expectedOutString := testhelper.Unix2dos(expectedOut.String())
+
 	_, stdout := mockIO()
 	err = Run(
 		[]string{path},
@@ -337,8 +341,8 @@ func TestRunDiff(t *testing.T) {
 	if err != nil {
 		t.Errorf("unexpected error: %s", err)
 	}
-	if !regexp.MustCompile(expectedOut.String()).Match(stdout.Bytes()) {
-		t.Errorf("stdout want match:\n%s\ngot:\n%q", expectedOut, stdout)
+	if !regexp.MustCompile(expectedOutString).Match(stdout.Bytes()) {
+		t.Errorf("stdout want match:\n%s\ngot:\n%q", expectedOutString, stdout)
 	}
 }
 
