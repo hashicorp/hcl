57d8ff0c1be7dd8b6733dd19f74d5136a0916a3f
Martin Atkins <mart@degeneration.co.uk>
2017-05-30 19:01:40 -0700

    json: update tokentype_string for recent new token types

--- zcl/json/tokentype_string.go
+++ zcl/json/tokentype_string.go
@@ -4,20 +4,21 @@ package json
 
 import "fmt"
 
-const _tokenType_name = "tokenInvalidtokenCommatokenColontokenKeywordtokenNumbertokenStringtokenBrackOtokenBrackCtokenBraceOtokenBraceCtokenEOF"
+const _tokenType_name = "tokenInvalidtokenCommatokenColontokenEqualstokenKeywordtokenNumbertokenStringtokenBrackOtokenBrackCtokenBraceOtokenBraceCtokenEOF"
 
 var _tokenType_map = map[tokenType]string{
 	0:    _tokenType_name[0:12],
 	44:   _tokenType_name[12:22],
 	58:   _tokenType_name[22:32],
-	75:   _tokenType_name[32:44],
-	78:   _tokenType_name[44:55],
-	83:   _tokenType_name[55:66],
-	91:   _tokenType_name[66:77],
-	93:   _tokenType_name[77:88],
-	123:  _tokenType_name[88:99],
-	125:  _tokenType_name[99:110],
-	9220: _tokenType_name[110:118],
+	61:   _tokenType_name[32:43],
+	75:   _tokenType_name[43:55],
+	78:   _tokenType_name[55:66],
+	83:   _tokenType_name[66:77],
+	91:   _tokenType_name[77:88],
+	93:   _tokenType_name[88:99],
+	123:  _tokenType_name[99:110],
+	125:  _tokenType_name[110:121],
+	9220: _tokenType_name[121:129],
 }
 
 func (i tokenType) String() string {
