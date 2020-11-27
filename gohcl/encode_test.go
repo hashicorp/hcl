dfea3a8654b3cdb11ecffdb473748dffd3da0756
Alex Sch <alexander@jexia.com>
2020-11-27 17:43:58 +0200

    allow interface/[]interface types to be encoded if interface is struct type

--- gohcl/encode_test.go
+++ gohcl/encode_test.go
@@ -8,6 +8,26 @@ import (
 )
 
 func ExampleEncodeIntoBody() {
+	type DynamicExample struct {
+		Foo string  `hcl:"foo"`
+		Bar float64 `hcl:"bar"`
+		Baz bool    `hcl:"baz"`
+	}
+	type HTTPOptions struct {
+		Listener string `hcl:"listener,label"`
+		Address  string `hcl:"address"`
+		Secure   bool   `hcl:"secure"`
+	}
+	type GRPCOptions struct {
+		Listener string `hcl:"listener,label"`
+		Address  string `hcl:"address"`
+	}
+	type MQTTOptions struct {
+		Listener string   `hcl:"listener,label"`
+		Address  string   `hcl:"address"`
+		Topics   []string `hcl:"topics"`
+	}
+
 	type Service struct {
 		Name string   `hcl:"name,label"`
 		Exe  []string `hcl:"executable"`
@@ -17,10 +37,12 @@ func ExampleEncodeIntoBody() {
 		Arch string `hcl:"arch"`
 	}
 	type App struct {
-		Name        string       `hcl:"name"`
-		Desc        string       `hcl:"description"`
-		Constraints *Constraints `hcl:"constraints,block"`
-		Services    []Service    `hcl:"service,block"`
+		Name        string         `hcl:"name"`
+		Desc        string         `hcl:"description"`
+		Constraints *Constraints   `hcl:"constraints,block"`
+		Services    []Service      `hcl:"service,block"`
+		Options     [3]interface{} `hcl:"option,block"`
+		Dynamic     interface{}    `hcl:"whatever,block"`
 	}
 
 	app := App{
@@ -39,6 +61,26 @@ func ExampleEncodeIntoBody() {
 				Name: "worker",
 				Exe:  []string{"./worker"},
 			},
+		},
+		Options: [3]interface{}{
+			HTTPOptions{
+				Listener: "http",
+				Address:  ":8080",
+			},
+			GRPCOptions{
+				Listener: "grpc",
+				Address:  ":5051",
+			},
+			MQTTOptions{
+				Listener: "mqtt",
+				Address:  ":1883",
+				Topics:   []string{"foo", "bar"},
+			},
+		},
+		Dynamic: DynamicExample{
+			Foo: "foo",
+			Bar: 42,
+			Baz: true,
 		},
 	}
 
@@ -61,4 +103,23 @@ func ExampleEncodeIntoBody() {
 	// service "worker" {
 	//   executable = ["./worker"]
 	// }
+	//
+	// option "http" {
+	//   address = ":8080"
+	//   secure  = false
+	// }
+	// option "grpc" {
+	//   address = ":5051"
+	// }
+	// option "mqtt" {
+	//   address = ":1883"
+	//   topics  = ["foo", "bar"]
+	// }
+	//
+	// whatever {
+	//   foo = "foo"
+	//   bar = 42
+	//   baz = true
+	// }
+
 }
