6decd62cf38f5c67f075ef02ecf859318235d036
Carson Black <uhhadd@gmail.com>
2020-12-12 23:13:37 -0500

    gohcl: Add support for optional-block
    optional-block allows for a block to be omitted when parsing an .hcl file

--- gohcl/schema.go
+++ gohcl/schema.go
@@ -152,6 +152,9 @@ func getFieldTags(ty reflect.Type) *fieldTags {
 			ret.Attributes[name] = i
 		case "block":
 			ret.Blocks[name] = i
+		case "optional-block":
+			ret.Blocks[name] = i
+			ret.Optional[name] = true
 		case "label":
 			ret.Labels = append(ret.Labels, labelField{
 				FieldIndex: i,
