ff690b9803a30863d0c96031567c9941ec67f178
incubator4 <aries0robin@gmail.com>
2021-12-22 18:13:13 +0800

    hclwrite: Various new "TokensFor..." functions
    The "TokensFor..." family of functions all aim to construct valid
    raw token sequences representing particular syntax constructs.
    
    Previously we had only "leaf" functions TokensForValue and
    TokensForTraversal, but nothing to help with constructing compound
    structures.
    
    Here we add TokensForTuple, TokensForObject, and
    TokensForFunctionCall which together cover all of the constructs
    that HCL allows static analysis of, and thus constructs where it's
    likely that someone would want to generate an expression that
    is interpreted purely by its syntax and not resolved into a value.
    
    What all of these have in common is that they take other Tokens
    values as arguments and include them verbatim as part of their
    result, with the caller being responsible for making sure these
    smaller units are themselves valid expression tokens.
    
    This also adds TokensForIdentifier as a convenient shorthand for
    generating single-identifier tokens, which is particularly useful
    for populating the attribute names in TokensForObject.

--- hclwrite/generate_test.go
+++ hclwrite/generate_test.go
@@ -536,3 +536,333 @@ func TestTokensForTraversal(t *testing.T) {
 		}
 	}
 }
+
+func TestTokensForTuple(t *testing.T) {
+	tests := map[string]struct {
+		Val  []Tokens
+		Want Tokens
+	}{
+		"no elements": {
+			nil,
+			Tokens{
+				{Type: hclsyntax.TokenOBrack, Bytes: []byte{'['}},
+				{Type: hclsyntax.TokenCBrack, Bytes: []byte{']'}},
+			},
+		},
+		"one element": {
+			[]Tokens{
+				TokensForValue(cty.StringVal("foo")),
+			},
+			Tokens{
+				{Type: hclsyntax.TokenOBrack, Bytes: []byte{'['}},
+				{Type: hclsyntax.TokenOQuote, Bytes: []byte(`"`)},
+				{Type: hclsyntax.TokenQuotedLit, Bytes: []byte("foo")},
+				{Type: hclsyntax.TokenCQuote, Bytes: []byte(`"`)},
+				{Type: hclsyntax.TokenCBrack, Bytes: []byte{']'}},
+			},
+		},
+		"two elements": {
+			[]Tokens{
+				TokensForTraversal(hcl.Traversal{
+					hcl.TraverseRoot{Name: "root"},
+					hcl.TraverseAttr{Name: "attr"},
+				}),
+				TokensForValue(cty.StringVal("foo")),
+			},
+			Tokens{
+				{Type: hclsyntax.TokenOBrack, Bytes: []byte{'['}},
+				{Type: hclsyntax.TokenIdent, Bytes: []byte("root")},
+				{Type: hclsyntax.TokenDot, Bytes: []byte(".")},
+				{Type: hclsyntax.TokenIdent, Bytes: []byte("attr")},
+				{Type: hclsyntax.TokenComma, Bytes: []byte{','}},
+				{Type: hclsyntax.TokenOQuote, Bytes: []byte(`"`), SpacesBefore: 1},
+				{Type: hclsyntax.TokenQuotedLit, Bytes: []byte("foo")},
+				{Type: hclsyntax.TokenCQuote, Bytes: []byte(`"`)},
+				{Type: hclsyntax.TokenCBrack, Bytes: []byte{']'}},
+			},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := TokensForTuple(test.Val)
+
+			if !cmp.Equal(got, test.Want) {
+				diff := cmp.Diff(got, test.Want, cmp.Comparer(func(a, b []byte) bool {
+					return bytes.Equal(a, b)
+				}))
+				var gotBuf, wantBuf bytes.Buffer
+				got.WriteTo(&gotBuf)
+				test.Want.WriteTo(&wantBuf)
+				t.Errorf(
+					"wrong result\nvalue: %#v\ngot:   %s\nwant:  %s\ndiff:  %s",
+					test.Val, gotBuf.String(), wantBuf.String(), diff,
+				)
+			}
+		})
+	}
+}
+
+func TestTokensForObject(t *testing.T) {
+	tests := map[string]struct {
+		Val  []ObjectAttrTokens
+		Want Tokens
+	}{
+		"no attributes": {
+			nil,
+			Tokens{
+				{Type: hclsyntax.TokenOBrace, Bytes: []byte{'{'}},
+				{Type: hclsyntax.TokenCBrace, Bytes: []byte{'}'}},
+			},
+		},
+		"one attribute": {
+			[]ObjectAttrTokens{
+				{
+					Name: TokensForTraversal(hcl.Traversal{
+						hcl.TraverseRoot{Name: "bar"},
+					}),
+					Value: TokensForValue(cty.StringVal("baz")),
+				},
+			},
+			Tokens{
+				{Type: hclsyntax.TokenOBrace, Bytes: []byte{'{'}},
+				{Type: hclsyntax.TokenNewline, Bytes: []byte{'\n'}},
+				{Type: hclsyntax.TokenIdent, Bytes: []byte("bar"), SpacesBefore: 2},
+				{Type: hclsyntax.TokenEqual, Bytes: []byte{'='}, SpacesBefore: 1},
+				{Type: hclsyntax.TokenOQuote, Bytes: []byte(`"`), SpacesBefore: 1},
+				{Type: hclsyntax.TokenQuotedLit, Bytes: []byte("baz")},
+				{Type: hclsyntax.TokenCQuote, Bytes: []byte(`"`)},
+				{Type: hclsyntax.TokenNewline, Bytes: []byte{'\n'}},
+				{Type: hclsyntax.TokenCBrace, Bytes: []byte{'}'}},
+			},
+		},
+		"two attributes": {
+			[]ObjectAttrTokens{
+				{
+					Name: TokensForTraversal(hcl.Traversal{
+						hcl.TraverseRoot{Name: "foo"},
+					}),
+					Value: TokensForTraversal(hcl.Traversal{
+						hcl.TraverseRoot{Name: "root"},
+						hcl.TraverseAttr{Name: "attr"},
+					}),
+				},
+				{
+					Name: TokensForTraversal(hcl.Traversal{
+						hcl.TraverseRoot{Name: "bar"},
+					}),
+					Value: TokensForValue(cty.StringVal("baz")),
+				},
+			},
+			Tokens{
+				{Type: hclsyntax.TokenOBrace, Bytes: []byte{'{'}},
+				{Type: hclsyntax.TokenNewline, Bytes: []byte{'\n'}},
+				{Type: hclsyntax.TokenIdent, Bytes: []byte("foo"), SpacesBefore: 2},
+				{Type: hclsyntax.TokenEqual, Bytes: []byte{'='}, SpacesBefore: 1},
+				{Type: hclsyntax.TokenIdent, Bytes: []byte("root"), SpacesBefore: 1},
+				{Type: hclsyntax.TokenDot, Bytes: []byte(".")},
+				{Type: hclsyntax.TokenIdent, Bytes: []byte("attr")},
+				{Type: hclsyntax.TokenNewline, Bytes: []byte{'\n'}},
+				{Type: hclsyntax.TokenIdent, Bytes: []byte("bar"), SpacesBefore: 2},
+				{Type: hclsyntax.TokenEqual, Bytes: []byte{'='}, SpacesBefore: 1},
+				{Type: hclsyntax.TokenOQuote, Bytes: []byte(`"`), SpacesBefore: 1},
+				{Type: hclsyntax.TokenQuotedLit, Bytes: []byte("baz")},
+				{Type: hclsyntax.TokenCQuote, Bytes: []byte(`"`)},
+				{Type: hclsyntax.TokenNewline, Bytes: []byte{'\n'}},
+				{Type: hclsyntax.TokenCBrace, Bytes: []byte{'}'}},
+			},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := TokensForObject(test.Val)
+
+			if !cmp.Equal(got, test.Want) {
+				diff := cmp.Diff(got, test.Want, cmp.Comparer(func(a, b []byte) bool {
+					return bytes.Equal(a, b)
+				}))
+				var gotBuf, wantBuf bytes.Buffer
+				got.WriteTo(&gotBuf)
+				test.Want.WriteTo(&wantBuf)
+				t.Errorf(
+					"wrong result\nvalue: %#v\ngot:   %s\nwant:  %s\ndiff:  %s",
+					test.Val, gotBuf.String(), wantBuf.String(), diff,
+				)
+			}
+		})
+	}
+}
+
+func TestTokensForFunctionCall(t *testing.T) {
+	tests := map[string]struct {
+		FuncName string
+		Val      []Tokens
+		Want     Tokens
+	}{
+		"no arguments": {
+			"uuid",
+			nil,
+			Tokens{
+				{Type: hclsyntax.TokenIdent, Bytes: []byte("uuid")},
+				{Type: hclsyntax.TokenOParen, Bytes: []byte{'('}},
+				{Type: hclsyntax.TokenCParen, Bytes: []byte(")")},
+			},
+		},
+		"one argument": {
+			"strlen",
+			[]Tokens{
+				TokensForValue(cty.StringVal("hello")),
+			},
+			Tokens{
+				{Type: hclsyntax.TokenIdent, Bytes: []byte("strlen")},
+				{Type: hclsyntax.TokenOParen, Bytes: []byte{'('}},
+				{Type: hclsyntax.TokenOQuote, Bytes: []byte(`"`)},
+				{Type: hclsyntax.TokenQuotedLit, Bytes: []byte("hello")},
+				{Type: hclsyntax.TokenCQuote, Bytes: []byte(`"`)},
+				{Type: hclsyntax.TokenCParen, Bytes: []byte(")")},
+			},
+		},
+		"two arguments": {
+			"list",
+			[]Tokens{
+				TokensForIdentifier("string"),
+				TokensForIdentifier("int"),
+			},
+			Tokens{
+				{Type: hclsyntax.TokenIdent, Bytes: []byte("list")},
+				{Type: hclsyntax.TokenOParen, Bytes: []byte{'('}},
+				{Type: hclsyntax.TokenIdent, Bytes: []byte("string")},
+				{Type: hclsyntax.TokenComma, Bytes: []byte(",")},
+				{Type: hclsyntax.TokenIdent, Bytes: []byte("int"), SpacesBefore: 1},
+				{Type: hclsyntax.TokenCParen, Bytes: []byte(")")},
+			},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := TokensForFunctionCall(test.FuncName, test.Val...)
+
+			if !cmp.Equal(got, test.Want) {
+				diff := cmp.Diff(got, test.Want, cmp.Comparer(func(a, b []byte) bool {
+					return bytes.Equal(a, b)
+				}))
+				var gotBuf, wantBuf bytes.Buffer
+				got.WriteTo(&gotBuf)
+				test.Want.WriteTo(&wantBuf)
+				t.Errorf(
+					"wrong result\nvalue: %#v\ngot:   %s\nwant:  %s\ndiff:  %s",
+					test.Val, gotBuf.String(), wantBuf.String(), diff,
+				)
+			}
+		})
+	}
+}
+
+func TestTokenGenerateConsistency(t *testing.T) {
+
+	bytesComparer := cmp.Comparer(func(a, b []byte) bool {
+		return bytes.Equal(a, b)
+	})
+
+	// This test verifies that different ways of generating equivalent token
+	// sequences all generate identical tokens, to help us keep them all in
+	// sync under future maintanence.
+
+	t.Run("tuple constructor", func(t *testing.T) {
+		tests := map[string]struct {
+			elems []cty.Value
+		}{
+			"no elements": {
+				nil,
+			},
+			"one element": {
+				[]cty.Value{
+					cty.StringVal("hello"),
+				},
+			},
+			"two elements": {
+				[]cty.Value{
+					cty.StringVal("hello"),
+					cty.StringVal("world"),
+				},
+			},
+		}
+
+		for name, test := range tests {
+			t.Run(name, func(t *testing.T) {
+				var listVal cty.Value
+				if len(test.elems) > 0 {
+					listVal = cty.ListVal(test.elems)
+				} else {
+					listVal = cty.ListValEmpty(cty.DynamicPseudoType)
+				}
+				fromListValue := TokensForValue(listVal)
+				fromTupleValue := TokensForValue(cty.TupleVal(test.elems))
+				elemTokens := make([]Tokens, len(test.elems))
+				for i, v := range test.elems {
+					elemTokens[i] = TokensForValue(v)
+				}
+				fromTupleTokens := TokensForTuple(elemTokens)
+
+				if diff := cmp.Diff(fromListValue, fromTupleTokens, bytesComparer); diff != "" {
+					t.Errorf("inconsistency between TokensForValue(list) and TokensForTuple\n%s", diff)
+				}
+				if diff := cmp.Diff(fromTupleValue, fromTupleTokens, bytesComparer); diff != "" {
+					t.Errorf("inconsistency between TokensForValue(tuple) and TokensForTuple\n%s", diff)
+				}
+
+			})
+		}
+	})
+
+	t.Run("object constructor", func(t *testing.T) {
+		tests := map[string]struct {
+			attrs map[string]cty.Value
+		}{
+			"no elements": {
+				nil,
+			},
+			"one element": {
+				map[string]cty.Value{
+					"greeting": cty.StringVal("hello"),
+				},
+			},
+			"two elements": {
+				map[string]cty.Value{
+					"greeting1": cty.StringVal("hello"),
+					"greeting2": cty.StringVal("world"),
+				},
+			},
+		}
+
+		for name, test := range tests {
+			t.Run(name, func(t *testing.T) {
+				var mapVal cty.Value
+				if len(test.attrs) > 0 {
+					mapVal = cty.MapVal(test.attrs)
+				} else {
+					mapVal = cty.MapValEmpty(cty.DynamicPseudoType)
+				}
+				fromMapValue := TokensForValue(mapVal)
+				fromObjectValue := TokensForValue(cty.ObjectVal(test.attrs))
+				attrTokens := make([]ObjectAttrTokens, 0, len(test.attrs))
+				for k, v := range test.attrs {
+					attrTokens = append(attrTokens, ObjectAttrTokens{
+						Name:  TokensForIdentifier(k),
+						Value: TokensForValue(v),
+					})
+				}
+				fromObjectTokens := TokensForObject(attrTokens)
+
+				if diff := cmp.Diff(fromMapValue, fromObjectTokens, bytesComparer); diff != "" {
+					t.Errorf("inconsistency between TokensForValue(map) and TokensForObject\n%s", diff)
+				}
+				if diff := cmp.Diff(fromObjectValue, fromObjectTokens, bytesComparer); diff != "" {
+					t.Errorf("inconsistency between TokensForValue(object) and TokensForObject\n%s", diff)
+				}
+			})
+		}
+	})
+}
