a3f04c414a7f4c35b33195be74a174de3aed6e17
Martin Atkins <mart@degeneration.co.uk>
2023-10-12 10:06:02 -0700

    ext/dynblock: Allow callers to veto for_each values
    Callers might have additional rules for what's acceptable in a for_each
    value for a dynamic block. For example, Terraform wants to forbid using
    sensitive values here because it would cause the expansion to disclose the
    length of the given collection.
    
    Therefore this provides a hook point for callers to insert additional
    checks just after the for_each expression has been evaluated and before
    any of the built-in checks are run.
    
    This introduces the "functional options" pattern for ExpandBlock for the
    first time, as a way to extend the API without breaking compatibility with
    existing callers. There is currently only this one option.

---
+++ ext/dynblock/options.go
@@ -0,0 +1,23 @@
+package dynblock
+
+import (
+	"github.com/hashicorp/hcl/v2"
+	"github.com/zclconf/go-cty/cty"
+)
+
+type ExpandOption interface {
+	applyExpandOption(*expandBody)
+}
+
+type optCheckForEach struct {
+	check func(cty.Value, hcl.Expression, *hcl.EvalContext) hcl.Diagnostics
+}
+
+func OptCheckForEach(check func(cty.Value, hcl.Expression, *hcl.EvalContext) hcl.Diagnostics) ExpandOption {
+	return optCheckForEach{check}
+}
+
+// applyExpandOption implements ExpandOption.
+func (o optCheckForEach) applyExpandOption(body *expandBody) {
+	body.checkForEach = append(body.checkForEach, o.check)
+}
