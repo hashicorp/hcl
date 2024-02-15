8dddc351e4d2782b3a62844f3c3f87d72f9f5e13
Radek Simko <radek.simko@gmail.com>
2024-02-15 16:02:44 +0000

    hclwrite: add two more test cases

--- hclwrite/format_test.go
+++ hclwrite/format_test.go
@@ -620,6 +620,14 @@ module "x" {
 			`attr = provider::framework::example()`,
 		},
 		{
+			`attr = provider :: framework :: example()`,
+			`attr = provider::framework::example()`,
+		},
+		{
+			`attr = provider ::framework:: example()`,
+			`attr = provider::framework::example()`,
+		},
+		{
 			// This is invalid syntax so formatting it with spaces
 			// does not have any meaning other than to make the fact more visible
 			`attr = provider::+example()`,
