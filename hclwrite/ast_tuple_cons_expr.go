b9f0a773032c950ff7c74dec5693c896025aabc7
Baraa Basata <baraa.basata@hashicorp.com>
2026-09-16 14:17:09 -0400

    Remove unused struct field

--- hclwrite/ast_tuple_cons_expr.go
+++ hclwrite/ast_tuple_cons_expr.go
@@ -1,3 +1,6 @@
+// Copyright IBM Corp. 2014, 2026
+// SPDX-License-Identifier: MPL-2.0
+
 package hclwrite
 
 func newTupleConsExpr() *TupleConsExpr {
@@ -10,6 +13,5 @@ func newTupleConsExpr() *TupleConsExpr {
 type TupleConsExpr struct {
 	inTree
 
-	exprs       nodeSet
-	expandFinal bool
+	exprs nodeSet
 }
